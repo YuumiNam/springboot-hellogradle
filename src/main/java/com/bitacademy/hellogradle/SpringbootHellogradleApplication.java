66154b2a79c0bbaf42d572f287bc109401091587
dltjsam <dltjsam@gmail.com>
2023-01-17 17:46:35 +0900

    springboot gradle #first init

---
+++ src/main/java/com/bitacademy/hellogradle/SpringbootHellogradleApplication.java
@@ -0,0 +1,11 @@
+package com.bitacademy.hellogradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHellogradleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHellogradleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bitacademy/hellogradle/SpringbootHellogradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitacademy.hellogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHellogradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
